edc2f1f3d38fda5fe84fcb76d5228fda6ad39026
chaeynz <chaeynz4msg@gmail.com>
2025-08-24 15:04:39 +0200

    chore: improve test script logging and add colors

--- test.sh
+++ test.sh
@@ -1,24 +1,37 @@
+#!/bin/bash
+
+_BOLD=$(tput bold)
+_RED=$(tput setaf 1)
+_GREEN=$(tput setaf 2)
+_CYAN=$(tput setaf 6)
+_CLEAR=$(tput sgr0)
+
 set -a
 source ./netbox_version.env
 set +a
 
+NETBOX_PLUGINS=($(python3 -c "import json; from configuration.configuration import PLUGINS; print(json.dumps(PLUGINS))" | jq -r '.[]'))
+
+echo "${_GREEN}INFO: Detected the following plugins in config: ${NETBOX_PLUGINS[@]}${_CLEAR}"
+
+echo "${_GREEN}INFO: Dumping plugin_requirements.txt:${_CLEAR}"
+cat ./plugin_requirements.txt
+
 docker compose -f docker-compose.test.yml -f docker-compose.test.override.yml up -d --build --wait --wait-timeout 900
 
-NETBOX_PLUGINS=($(python3 -c "import json; from configuration.configuration import PLUGINS; print(json.dumps(PLUGINS))" | jq -r '.[]'))
 NETBOX_STATUS=$(curl -s http://127.0.0.1:8000/api/status/)
 
-
-for i in ${NETBOX_PLUGINS[@]}; do
-  RESULT=$(echo $NETBOX_STATUS | jq -r --arg key "$i" '."installed-apps" | has($key)')
+for plugin in ${NETBOX_PLUGINS[@]}; do
+  RESULT=$(echo $NETBOX_STATUS | jq -r --arg key "$plugin" '."installed-apps" | has($key)')
   if $RESULT; then
-    echo "INFO: Plugin $i found"
+    echo "${_GREEN}INFO: Plugin $plugin found in Netbox deployment${_CLEAR}"
   else
-    echo "ERROR: Plugin $i was not found in /api/status/" >&2
+    echo "${_RED}ERROR: Plugin $plugin was not found in /api/status/${_CLEAR}" >&2
     exit 1
   fi
 done
 
-echo "INFO: SUCCESS!! All tests passed"
+echo "${_GREEN}INFO: SUCCESS!! All tests passed${_CLEAR}"
 
 docker compose -f docker-compose.test.yml down -v
 docker compose -f docker-compose.test.yml rm
