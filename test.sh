9d0f465c352afcacaaf8124dcf827c1592a19869
chaeynz <chaeynz4msg@gmail.com>
2025-08-24 04:51:32 +0200

    fix: increase timeouts and start period for test deployment (database migrations are taking long)

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ set -a
 source ./netbox_version.env
 set +a
 
-docker compose -f docker-compose.test.yml -f docker-compose.test.override.yml up -d --build --wait --wait-timeout 600
+docker compose -f docker-compose.test.yml -f docker-compose.test.override.yml up -d --build --wait --wait-timeout 900
 
 NETBOX_PLUGINS=($(python3 -c "import json; from configuration.configuration import PLUGINS; print(json.dumps(PLUGINS))" | jq -r '.[]'))
 NETBOX_STATUS=$(curl -s http://127.0.0.1:8000/api/status/)
